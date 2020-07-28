ed4ed24e42bfefa0253567e7598feaab53d70dfc
Antonio Carlos R Andretta <antonioandretta@uol.com.br>
2020-07-28 20:56:51 -0300

    ajustes.  Pré slider

--- src/components/menu/index.js
+++ src/components/menu/index.js
@@ -1,17 +1,19 @@
 import React from 'react';
+import { Link } from 'react-router-dom' ;
 import Logo from '../../assets/img/Logo.png';
 import './Menu.css';
-import ButtonLink from './components/ButtonLink';
+import Button from '../../components/Button';
+//import ButtonLink from './components/ButtonLink';
 
 function Menu() {
     return (
         <nav className="Menu">
-          <a href="/">   
+          <Link to="/">   
             <img className="Logo" src={Logo} alt="Logo Aluraflix" /> 
-          </a>
-          <ButtonLink className="ButtonLink" href="/"> 
+          </Link>
+          <Button as={Link }className="ButtonLink" to="/cadastro/Video"> 
           Novo Vídeo
-          </ButtonLink>
+          </Button>
     </nav>
     );
 }
