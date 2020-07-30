43df77021f5ac1b3d41cc297fcf1904bdda71831
Antonio Carlos R Andretta <antonioandretta@uol.com.br>
2020-07-30 19:20:45 -0300

    ajuste URLS

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -30,18 +30,18 @@ function CadastroCategoria() {
   }
 
   useEffect(() => {
-    if (window.location.href.includes('localhost')) {
-      const URL = 'http://localhost:8080/categorias';
-      fetch(URL)
-        .then(async (respostaDoServer) =>{
-          if (respostaDoServer.ok) {
-            const resposta = await respostaDoServer.json();
-            setCategorias(resposta);
-            return; 
-          }
-          throw new Error('Não foi possível pegar os dados');
-        })
-    }
+    const URL =  window.location.href.includes('localhost') 
+      ? 'https://hobbyflix.herokuapp.com/categorias' 
+      : 'http://localhost:8080/categorias';
+    fetch(URL)
+      .then(async (respostaDoServer) =>{
+        if (respostaDoServer.ok) {
+          const resposta = await respostaDoServer.json();
+          setCategorias(resposta);
+          return; 
+        }
+        throw new Error('Não foi possível pegar os dados');
+      });
   }, []);
   return (
     <PageDefault>
